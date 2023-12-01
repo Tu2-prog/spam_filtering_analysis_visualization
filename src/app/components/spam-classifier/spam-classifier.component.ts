b5bd6a6afe1fcc529c7cb56fef50961055446ada
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-01 22:46:10 +0100

    set up component for classifier and adapter for requests

---
+++ src/app/components/spam-classifier/spam-classifier.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spam-classifier',
+  templateUrl: './spam-classifier.component.html',
+  styleUrl: './spam-classifier.component.css'
+})
+export class SpamClassifierComponent {
+
+}
