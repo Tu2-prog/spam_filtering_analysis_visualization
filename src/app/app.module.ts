93b60f6d94779cd284a19596a641f9bbab3dea4f
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-02 21:39:23 +0100

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpamClassifierComponent } from './components/spam-classifier/spam-classifier.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SpamClassifierComponent
+    SpamClassifierComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
