6e4fa89f2249d1965ce4d2a4d452e32f3e2a089f
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-02 22:28:34 +0100

    add text field for classification later on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpamClassifierComponent } from './components/spam-classifier/spam-classifier.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spam_filtering_analysis_visualization';
+  onConfirmClick(spamClassifier: SpamClassifierComponent): void {
+    if (spamClassifier) {
+      const textAreaValue = spamClassifier.getTextAreaValue();
+      console.log('Text in the textarea:', textAreaValue);
+      // Perform any other actions with the text as needed
+    }
+  }
 }
--- src/app/components/spam-classifier/spam-classifier.component.ts
+++ src/app/components/spam-classifier/spam-classifier.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-spam-classifier',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './spam-classifier.component.css'
 })
 export class SpamClassifierComponent {
+  @ViewChild('textAreaRef') textAreaRef!: ElementRef;
 
+  // You can expose a method to get the textarea value if needed
+  getTextAreaValue(): string {
+    return this.textAreaRef.nativeElement.value;
+  }
 }
